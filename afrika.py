409f14cad9a1d0ed2de3e6f612cdd6655e2cafbc
ecdl <ecdl@suli.local>
2021-12-13 00:12:38 -0900

    comment

--- afrika.py
+++ afrika.py
@@ -1,6 +1,6 @@
 #HOZZ LÉTRE EGY LISTÁT ÉS A TARTALMÁT ÍRD KI AZ AFRIKA.TXT ÁLLOMÁNYBA
 
-afrika=["párduc", "oroszlán", "gorilla", "makákó", "bambusznád", "majomkenyérfa", "kókuszdió"]
+afrika=["párduc", "oroszlán", "gorilla", "makákó", "bambusznád", "majomkenyérfa", "kókuszdió", "ludas"]
 celfajl=open("afrika.txt" ,"w", encoding="utf-8")
 afrika.append("szavannák")
 afrika.append("fekete nők")
